4465088b27b788fe633d5186a21d99e5ceba4062
xienaiwen <sienaiwun@gmail.com>
2016-12-03 21:23:55 +0800

    frame

---
+++ luaParser/luaParser/PyLuaTblParser.py
@@ -0,0 +1,16 @@
+class PyLuaTblParser:
+    def load(self, s):
+        pass
+    def dump(self):
+        pass
+    def loadLuaTable(self, f):
+        pass
+    def dumpLuaTable(self, f):
+        pass
+    def loadDict(self, d):
+        pass
+    def dumpDict(self):
+        pass 
+
+    
+test_str = '{array = {65,23,5,},dict = {mixed = {43,54.33,false,9,string = "value",},array = {3,6,4,},string = "value",},}'
