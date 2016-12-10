c129d47798c44c3186864d19f231cf79f545923c
xienaiwen <sienaiwun@gmail.com>
2016-12-10 23:52:11 +0800

    update

--- luaParser/luaParser/parser.py
+++ luaParser/luaParser/parser.py
@@ -6,6 +6,8 @@ class Parser(object):
     def __init__(self):
         self._ch = ''
         self._at = 0
+        self._depth = 0
+        self._index = 0
         pass
 
     def eat_char(self,c):
@@ -18,7 +20,7 @@ class Parser(object):
             return
         self._text = text
         self._len = len(text)
-        self._depth = 0
+        
         self.next_char()
         return self.text_value()
        
@@ -41,18 +43,27 @@ class Parser(object):
             if self.match_whie():
                 self.next_char()
             break;
-    
+    #解析一段文字符号的值
     def text_value(self):
         self.skip_white()
         if self.eat_char('{'):
             return self.note()
         elif self._ch in [r'\'',r'"',r'[']:
-            return self.make_string(self._ch);
+            return self.make_bracket_string(self._ch);
         elif self._ch.isdigit() or self._ch == '-':
             return self.make_digit()
-        else:    
-            pass;
-    def make_string(self,endChar):
+        elif self._ch.isalpha():    
+            return self.make_word();
+        raise ParerError('value no matched pattern')
+
+    def make_word(self):
+        intStr = ''
+        while self._ch and self._ch.isalnum():
+            intStr += self._ch;    
+            self.next_char()
+        return intStr
+
+    def make_bracket_string(self,endChar):
         start_char = self._ch
         if endChar == r'[':
             endChar = r']'
@@ -88,22 +99,51 @@ class Parser(object):
                 return float(num_str)
         except:
             pass
-        
+    def isWord(ch):
+        if ch.isalpha() or ch.isdigit():
+            return True
+        return False
+
     def note(self):
         dict = {}
         index = 0
-        self.depth += 1
+        self._depth += 1
         if self.eat_char('}'):
             self._depth -= 1
             return dict
-
-
-        pass
+        else:
+            while self._ch:
+                    self.skip_white()
+                    if self.eat_char('}'):
+                        if key is not None:
+                            dict[self._index] = key
+                            self._index += 1
+                        if len( [key for key in dict.keys() if isinstance(key,int)]) ==len(dict):
+                            ar = []
+                            for key in dict:
+                                ar.insert(key,dict[key])
+                            dict = ar
+                        self._depth -= 1
+                        return dict
+                    if  self._ch == ',':
+                        continue;
+                    else:
+                        key = self.text_value()
+                        self.skip_white()
+                        if self._ch in ['=',',']:
+                            if self.eat_char('='):
+                                value = self.text_value()
+                                dict[key] = value
+                            elif self.eat_char(','):
+                                dict[self._index] = key
+                                self._index += 1
+                            key = None
+            raise ParerError('note Error')
 
 
 if __name__ == '__main__':
-    test_str = '{array = {65,23,5,},dict = {mixed = {43,54.33,false,9,string = "value",},array = {3,6,4,},string = "value",},}'
+    test_str = '{array = {65,23,5}}'
     digit_arar = '[-65.22]'
     a1 = Parser()
-    a= a1.parse_text(digit_arar)
+    a= a1.parse_text(test_str)
     print a
